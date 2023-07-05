6f00c61bf63aa4d1de8c582e40bcc099af56c7af
Wildson Seguchi <wseguchi@gmail.com>
2023-07-05 16:49:09 -0500

    App.jsx: included components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Cards from './components/Cards';
+import TextInput from './components/TextInput';
+import Method from './components/Method';
+import About from './components/About';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <h1>Texted</h1>
+      <Header />
+      <Hero />
+      <Cards />
+      <TextInput />
+      <Method />
+      <About />
+      <Footer />
     </div>
   );
 };
