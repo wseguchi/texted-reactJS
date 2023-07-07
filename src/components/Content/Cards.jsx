ce68b1932cb441e7c90d28e0a1d106b253105ea9
Wildson Seguchi <wseguchi@gmail.com>
2023-07-07 14:39:59 -0500

    Done Cards

--- src/components/Content/Cards.jsx
+++ src/components/Content/Cards.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import styles from './Content.module.css';
+import { FaBookOpenReader, FaCopy } from 'react-icons/fa6';
 
 const Cards = () => {
-  return <div>Cards</div>;
+  return (
+    <div className={`${styles.cardsMain} width-restrainer`}>
+      <div>
+        <FaBookOpenReader className={styles.cardsIcon} />
+        <h1>
+          (<span className="strong-purple">read</span>)ability
+        </h1>
+        <h3 className={styles.cardsText}>
+          Check <strong>readability</strong> to keep your content accessible and
+          maximize your audience readership.
+        </h3>
+      </div>
+      <div>
+        <FaCopy className={styles.cardsIcon} />
+        <h1>
+          (<span className="strong-purple">similar</span>)ity
+        </h1>
+        <h3 className={styles.cardsText}>
+          Check <strong>similarity</strong> to avoid redundancies and
+          plagiarism.
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './Content.module.css';
+import Cards from './Cards';
 
 const Content = () => {
-  return <section id="content" className={styles.content}></section>;
+  return (
+    <section id="content" className={styles.content}>
+      <Cards />
+    </section>
+  );
 };
 
 export default Content;
